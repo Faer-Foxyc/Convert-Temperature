44f491b7bf6a9bf27d95e441468e682761bf0c98
Anton Fox <42889325+AntonFoxy@users.noreply.github.com>
2023-04-26 21:02:36 +0300

    Добавьте файлы проекта.

---
+++ Classes/Convert_Class.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Convert_Temperature.Classes
+{
+    internal class Convert_Class
+    {
+        public static int numbGradus { get; set; }
+
+        public Convert_Class(int _numbGradus)
+        {
+            numbGradus = _numbGradus;
+        }
+
+        public List<string> Convert_Temperature()
+        {
+            var list = new List<string>();
+            
+            var sumFahrenheit = numbGradus * 1.8 + 32;
+            var sumCelsius = (numbGradus - 32) / 1.8;
+
+            list.Add($" {numbGradus}C = {sumFahrenheit} F");
+            list.Add($" {numbGradus}F = {sumCelsius} C");
+
+            return list;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using Convert_Temperature.Classes;
+using System;
+using System.Collections.Generic;
+
+namespace Convert_Temperature
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                var head = "Calculation of degrees Celsius and Fahrenheit";
+                Console.WriteLine($"\n\t{head.ToUpper()}");
+                Console.WriteLine("\t---------------------------------------------");
+                Console.Write(" Enter numb: ");
+                var numb = Console.ReadLine();
+                Console.WriteLine();
+
+                try
+                {
+                    Convert_Class convert_Class = new Convert_Class(Convert.ToInt32(numb));
+                    List<string> list = convert_Class.Convert_Temperature();
+                    foreach (var item in list)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine(e.Message);
+                }
+
+                Console.WriteLine();
+                Console.Write(" Do you want continue Yes/No - ");
+                var answer = Console.ReadLine();
+                Console.WriteLine();
+
+                if (answer.ToLower() == "no")
+                {
+                    Console.WriteLine(" Good bye");
+                    break;
+                }
+            }
+        }
+    }
+}
